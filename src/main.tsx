3b131398de4b43198257e28eeefc0665d3eeab51
George Salebe <108626837+Georgesa98@users.noreply.github.com>
2026-03-16 17:43:32 +0300

    Change BrowserRouter basename to root path

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter basename="/Portfolio">
+    <BrowserRouter basename="/">
       <App />
     </BrowserRouter>
   </StrictMode>,
